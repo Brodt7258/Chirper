666118c506e994d8bb191345948f098c5f6719af
Tim Dill <brodt7258@gmail.com>
2018-05-05 15:28:08 -0400

    set up initial actions and reducers

---
+++ src/actions/shared.js
@@ -0,0 +1,15 @@
+import { getInitialData } from '../utils/api';
+import { receiveUsers } from './users';
+import { receiveTweets } from './tweets';
+import { setAuthedUser } from './authedUser';
+
+const AUTHED_ID = 'tylermcginnis';
+
+export const handleInitialData = () => (dispatch) => {
+  return getInitialData()
+    .then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+}
